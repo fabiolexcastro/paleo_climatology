8bfe7e862e59b56d8342eb486b8d17bd81a02041
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-08-18 13:05:15 -0500

    Meaning

---
+++ 03 to calculate mean.R
@@ -0,0 +1,15 @@
+
+
+# Load libraries ----------------------------------------------------------
+require(pacman)
+pacman::p_load(terra, sf, tidyverse, rgeos, gtools, fs, glue)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+options(scipen = 999, warn = -1)
+
+# Load data ---------------------------------------------------------------
+setwd('E:/climate_article')
+
+dirs <- dir_ls('SilvanaData')
+print(dirs)
